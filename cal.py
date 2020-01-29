be20d066e5da1586d64138b57ac25ea63a95b604
Ragavi18 <60418609+Ragavi18@users.noreply.github.com>
2020-01-29 10:51:34 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    z=x+y
+    print(z)
+def sub(x,y):
+    z=x-y
+    print(z)
+def mul(x,y):
+    z=x*y
+    print(z)
+def div(x,y):
+    z=x/y
+    print(z)
+add(30,20)
+sub(32,45)
+mul(23,32)
+div(78,2)
